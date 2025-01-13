615b397b0872abecb8dc3a486d4212df100c2ff3
itashki <iliatashkinov9@gmail.com>
2025-01-13 13:56:21 +0000

    fix exports

--- lib/components/DropDown/index.ts
+++ lib/components/DropDown/index.ts
@@ -4,10 +4,12 @@ import {
 } from "@radix-ui/react-dropdown-menu";
 import { Content } from "./Content";
 import { Separator } from "./Separator";
+import { Item } from "./Item";
 
 export const DropDown = {
   Root: DropdownMenu,
   Trigger: DropdownMenuTrigger,
   Separator,
   Content,
+  Item,
 };
