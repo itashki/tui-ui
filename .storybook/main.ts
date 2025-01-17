504bc38cb2f3d1f12470c5b7e84152b559ee1652
itashki <iliatashkinov9@gmail.com>
2025-01-17 21:19:00 +0000

    strybook fix

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -3,7 +3,6 @@ import type { StorybookConfig } from "@storybook/react-vite";
 const config: StorybookConfig = {
   stories: ["../lib/**/stories.tsx"],
   addons: [
-    "@storybook/addon-onboarding",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions"
   ],
